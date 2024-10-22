87f469474fe6c6d90bfa964a6ac6cc5157b1f95f
merabisamyurashvili <158589735+merabisamyurashvili@users.noreply.github.com>
2024-10-22 21:25:58 +0400

    ddd

--- ticket/src/components/auth/register/MainRegister.tsx
+++ ticket/src/components/auth/register/MainRegister.tsx
@@ -1,7 +1,30 @@
+"use client";
+
 import Link from "next/link";
 import SociaBtn from "../SociaBtn";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useForm } from "react-hook-form";
+import * as val from "@/utils/validationGe";
+type Input = {
+  email: string;
+  checkbox: boolean;
+};
 
 export default function MainRegister() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Input>();
+
+  const route = useRouter();
+
+  function onSubmit(data: Input) {
+    console.log(data);
+    route.push("/auth/confirmEmail");
+  }
+
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="text-center font-semibold text-[16px]">რეგისტრაცია</h1>
@@ -13,27 +36,52 @@ export default function MainRegister() {
         <span>ან გააგრძელე</span>{" "}
         <hr className="w-[30%] border-1 border-black" />
       </div>
-      <form className=" flex flex-col gap-[16px] mt-[30px] sm:mt-[37px] w-[295px]">
+      <form
+        className=" flex flex-col gap-[16px] mt-[30px] sm:mt-[37px] w-[295px] "
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <label>
           <h2 className="text-[14px] font-medium ml-[8px]">ელ.ფოსტა</h2>
           <input
             className="h-[45px] w-full border-[1px] rounded-[12px] input-shadow pl-[18px] mt-[8px] placeholder:text-[14px] "
             placeholder="შეიყვანეთ თქვენი ელ.ფოსტა"
             type="email"
+            {...register("email", {
+              required: val.requiredMsg,
+              pattern: {
+                value: val.emailPattern,
+                message: val.emailErrorMsg,
+              },
+              maxLength: {
+                value: 254,
+                message: val.emailMaxLengthMsg,
+              },
+            })}
           />
+          {errors.email && (
+            <span className="text-red-600">{errors.email.message}</span>
+          )}
         </label>
 
         <label className="flex mt-[10px] items-center ">
-          <input className="w-[20px] h-[20px] rounded-[6px]" type="checkbox" />
+          <input
+            className="w-[20px] h-[20px] rounded-[6px]"
+            type="checkbox"
+            {...register("checkbox", {
+              required: val.requiredMsg,
+            })}
+          />
+          {errors.checkbox && (
+            <span className="text-red-600 ">{errors.checkbox.message}</span>
+          )}
+
           <h2 className="text-[14px] font-medium ml-[8px] opacity-70">
             ვეთანხმები
             <Link className="text-[#007AFF] underline" href={"#"}>
-              {" "}
-              წესებს / პირობებს{" "}
-            </Link>{" "}
-            და{" "}
+              წესებს / პირობებს
+            </Link>
+            და
             <Link className="text-[#007AFF] underline" href={"#"}>
-              {" "}
               კონფიდენციალურობის პოლიტიკას{" "}
             </Link>
           </h2>
